c69f4d6537038ba7ffc2946f5437cdccc25d9530
luana ferreira <luana.sf@aluno.ufop.edu.br>
2026-07-17 14:42:09 -0300

    update: inclusão de lógica de seleção de arquivo

--- src/App.java
+++ src/App.java
@@ -1,12 +1,55 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         try {
 
-            String caminho = "instancias/instancia_1000_200000.txt";
+            String caminhoDiretorio = "instancias";
+            File diretorio = new File(caminhoDiretorio);
+
+            File[] arquivos = diretorio.listFiles(File::isFile);
+
+            if (arquivos == null || arquivos.length == 0) {
+            System.out.println("Nenhum arquivo encontrado na pasta '" + caminhoDiretorio + "'.");
+            System.out.println("Certifique-se de que a pasta existe e contém os arquivos .txt.");
+            return;
+            }
+
+            System.out.println("SELEÇÃO DE INSTÂNCIAS DE TESTE");
+            System.out.println("------------------------------------------------------");
+
+            for (int i = 0; i < arquivos.length; i++) {
+            System.out.println((i + 1) + " - " + arquivos[i].getName());
+            }
             
-            Grafo grafo = LerGrafo.carregarGrafo(caminho);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\nDigite o número do arquivo que deseja carregar: ");
+
+            
+            String caminhoArquivo = "";
+
+            while (true) {
+                int opcao = scanner.nextInt();
+
+                if (opcao >=1 && opcao <= arquivos.length){
+
+                caminhoArquivo = arquivos[opcao-1].getPath();
+                System.out.println("\nArquivo selecionado com sucesso: " + caminhoArquivo);
+                break;
+                
+                } else {
+
+                    System.out.println("\n Opção inválida! Digite um número de arquivo válido.");
+
+                }
+            }
+
+            scanner.close();
+
+
+            Grafo grafo = LerGrafo.carregarGrafo(caminhoArquivo);
 
             int[] cores = new int[grafo.getNumVertices()];
             //tempo
@@ -18,7 +61,10 @@ public class App {
 
             double tempoSegundos = (fim - inicio) / 1_000_000_000.0;
 
-            System.out.println("Arquivo: " + caminho);
+            System.out.println("\nDADOS COLETADOS NA EXECUÇÃO DA INSTÂNCIA DE TESTE ");
+            System.out.println("------------------------------------------------------");
+
+            System.out.println("Arquivo: " + caminhoArquivo);
             System.out.println("Vértices: " + grafo.getNumVertices());
             System.out.println("Cores usadas: " + numCores);
             System.out.println("Tempo de execução: " + tempoSegundos + "s");
